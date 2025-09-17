f14140df081eae7fc6ecd181aefcc83bd34423b6
JONATAN NAIFELD <49549062@ort>
2025-09-17 09:04:21 -0300

    terminamos ej 1

--- ejercicios.js
+++ ejercicios.js
@@ -4,18 +4,42 @@ const fechaNac = document.getElementById("fechaNac")
 const resultado = document.getElementById("resultado")
 const formEdad = document.getElementById('formEdad')
 
-function CalcularEdad(fechaNac) {
+formEdad.addEventListener('submit', e => {
+    e.preventDefault()
     const hoy = new Date()
+    const nacimiento = new Date(fechaNac.value)
+    let edad = hoy.getFullYear() - nacimiento.getFullYear()
+    
+    const mesNac = nacimiento.getMonth()
+    const diaNac = nacimiento.getDate()
+    if (hoy.getMonth() < mesNac || (hoy.getMonth() === mesNac && hoy.getDate() < diaNac)) {
+        edad--
+    }
 
-    let edad = hoy - fechaNac
+    if (edad == 0){
+        edad = hoy.getMonth() - mesNac
+        if (hoy.getMonth() === mesNac && hoy.getDate() < diaNac) {
+            edad--
+        }
 
-    resultado.textContent = `Hola ${nombre}, tenés ${edad}`
-}
+        resultado.textContent = `Hola ${nombre.value}, tenés ${edad} mes(es)`
+    }
+    else if(edad < 0)
+    {
+        resultado.textContent = `Todavía no pasó esa fecha!`
+    }
+    else resultado.textContent = `Hola ${nombre.value}, tenés ${edad} año(s)`
+})
 
-formEdad.addEventListener('submit', () => {
-    const hoy = new Date()
+// 2
+const frutas = ["banana", "melón", "frutilla", "fruta", "damasco", "tomate", "kiwi", "sandía", "limón", "mora"]
+const fruta = document.getElementById("fruta")
+const resultado2 = document.getElementById("resultado2")
+const formFrutas = document.getElementById('formFrutas')
 
-    let edad = hoy - fechaNac
-
-    resultado.textContent = `Hola ${nombre}, tenés ${edad}`
+formFrutas.addEventListener('submit', e => {
+    e.preventDefault
+    console.log(fruta.value)
+    if(frutas.includes(fruta.value)) resultado2.textContent = `Si, tenemos ${fruta.value}`
+    else resultado2.textContent = `No tenemos ${fruta.value}`
 })
---
+++ utils.js
@@ -0,0 +1,8 @@
+// 3
+const comp1 = (10 == '10')
+const comp2 = (10 === '10')
+const tipo = 10.6
+const ver = (true == 1)
+
+console.log(`Ejercicio 3:`, comp1, comp2, typeof tipo, ver)
+
